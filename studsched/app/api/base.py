c5a0ddbcf145be8b149180fb8d6a0d36e18b1132
Jakub Stacherski <o503@interia.eu>
2024-04-08 14:21:36 +0200

    Simple subjects list response

--- studsched/app/api/base.py
+++ studsched/app/api/base.py
@@ -1,7 +1,12 @@
 """Endpoints for getting version information."""
+
 from typing import Any
 from fastapi import APIRouter
-from ..schemas.base import VersionResponse
+from ..schemas.base import (
+    VersionResponse,
+    SubjectListResponse,
+    SubjectResponse,
+)
 from ..version import __version__
 
 base_router = APIRouter()
@@ -16,3 +21,10 @@ async def version() -> Any:
         VersionResponse: A json response containing the version number.
     """
     return VersionResponse(version=__version__)
+
+
+@base_router.get("/subjects", response_model=SubjectListResponse)
+async def henlo() -> Any:
+    subjects = []
+    subjects.append(SubjectResponse(id="1", name="ZPRP"))
+    return SubjectListResponse(subjects=subjects)
--- tests/test_api/test_base.py
+++ tests/test_api/test_base.py
@@ -5,3 +5,9 @@ def test_get_version(test_client):
     response = test_client.get("/api/v1/version")
     assert response.status_code == 200
     assert response.json() == {"version": __version__}
+
+
+def test_subjects(test_client):
+    response = test_client.get("/api/v1/subjects")
+    assert response.status_code == 200
+    assert response.json() == {"subjects": [{"id": "1", "name": "ZPRP"}]}
