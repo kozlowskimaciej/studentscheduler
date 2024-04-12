3e8d37ad72125d77b4d02e121fc1c5c9c8f6ccec
Jakub Stacherski <o503@interia.eu>
2024-04-12 14:29:22 +0200

    Subjects endpoint using real db; filled_db fixture

--- studsched/app/api/base.py
+++ studsched/app/api/base.py
@@ -3,24 +3,21 @@
 from typing import Any
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-from datetime import datetime
 
 from ..db.queries import queries
 from ..db.session import SessionLocal
 from ..version import __version__
-from ..schemas.base import (
-    VersionResponse,
-    SubjectResponse,
-    SubjectStatus,
-    Task,
-    TaskType,
-    Requirement,
-    RequirementCreate,
-    RequirementType,
-    ThresholdType,
-)
+from ..schemas.base import VersionResponse, Subject, RequirementCreate
 
 base_router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @base_router.get("/version", response_model=VersionResponse)
@@ -34,42 +31,9 @@ async def version() -> Any:
     return VersionResponse(version=__version__)
 
 
-@base_router.get("/subjects", response_model=list[SubjectResponse])
-async def subjects() -> Any:
-    tasks = [
-        Task(
-            max_points=10,
-            deadline=datetime(2002, 1, 27, 1),
-            task_type=TaskType.LAB,
-        )
-    ]
-    requirements = [
-        Requirement(
-            id=0,
-            task_type=TaskType.LAB,
-            requirement_type=RequirementType.TOTAL,
-            threshold=5,
-            threshold_type=ThresholdType.POINTS,
-        )
-    ]
-    subjects = [
-        SubjectResponse(
-            id="1",
-            name="ZPRP",
-            status=SubjectStatus.PASSED,
-            tasks=tasks,
-            requirements=requirements,
-        )
-    ]
-    return subjects
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@base_router.get("/subjects", response_model=list[Subject])
+async def subjects(db: Session = Depends(get_db)) -> Any:
+    return queries.get_subjects(db)
 
 
 @base_router.post("/subjects/{subject_id}/requirements")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
-import pytest
+from sqlalchemy.orm import Session
 from fastapi.testclient import TestClient
+import pytest
+
 from studsched.app.application import create_application
 from studsched.app.db.session import engine, SessionLocal
 from studsched.app.db.base import Base
+from studsched.app.db.models import models
+from studsched.app.schemas import base as schemas
+
+
 @pytest.fixture
 def test_client():
     app = create_application()
@@ -18,3 +24,22 @@ def db_session():
     session.close()
     Base.metadata.drop_all(engine)
     engine.dispose()
+
+
+@pytest.fixture
+def filled_db(db_session: Session):
+    linked_course = models.LinkedCourse()
+    db_session.add(linked_course)
+    db_session.commit()
+    db_session.refresh(linked_course)
+
+    requirement = models.Requirement(
+        task_type=schemas.TaskType.LAB,
+        requirement_type=schemas.RequirementType.TOTAL,
+        threshold=5,
+        threshold_type=schemas.ThresholdType.POINTS,
+        linked_course_id=linked_course.id,
+    )
+    db_session.add(requirement)
+    db_session.commit()
+    yield db_session
