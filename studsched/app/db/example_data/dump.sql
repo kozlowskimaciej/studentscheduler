5e2eae85c63f099bcf9b4410e3b676c895e4801a
Adam <a.gepard007@gmail.com>
2024-04-14 22:27:01 +0200

    Changed initial app creation

---
+++ studsched/app/db/example_data/dump.sql
@@ -0,0 +1,16 @@
+-- SQL Dump
+
+-- Insert into tasks
+-- INSERT INTO tasks (max_points, result, deadline, task_type, ended, description) 
+-- VALUES (100, 90, '2022-12-31 23:59:59', 1, false, 'Task description');
+
+-- Insert into subjects
+-- INSERT INTO subjects (id, name, status) 
+-- VALUES ('1', 'Subject Name', 1);
+
+-- Insert into linked_course
+-- INSERT INTO linked_course (id) VALUES (1);
+SELECT * FROM linked_course;
+-- Insert into requirements
+-- INSERT INTO requirements (id, task_type, requirement_type, threshold, threshold_type, linked_course_id) 
+-- VALUES (1, 'LAB', 'TOTAL', 10, 'PERCENT', 1);
